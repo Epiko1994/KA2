5983393ad8cc9cfe28f0041f198538fc4e89f4df
anirml <jeppelj@yahoo.dk>
2019-10-13 09:58:38 +0200

    test corrected to work on droplet a

--- src/test/java/rest/PersonResourceTest.java
+++ src/test/java/rest/PersonResourceTest.java
@@ -28,6 +28,7 @@ public class PersonResourceTest {
 
     private static final int SERVER_PORT = 7777;
     private static final String SERVER_URL = "http://localhost/api";
+    
     private static Person r1,r2;
     
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
